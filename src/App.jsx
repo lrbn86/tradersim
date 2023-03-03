d22c604d48ce927bb00c6b2571c01262b1357d12
lrbn86 <lrbn86@gmail.com>
2023-03-03 00:10:29 -0500

    Test out other firebase functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from 'react';
-import { getDocsFromCollection } from './firebase/functions';
+import { getDocsFromCollection, addDocToCollection, updateDocFromCollection } from './firebase/functions';
 import './App.css';
+import Stock from './Stock';
 
 function App() {
+
   const [docs, setDocs] = useState([]);
+
   useEffect(() => {
     getDocsFromCollection('accounts').then((data) => setDocs(data));
+    // addDocToCollection({ Ask: 200, Bid: 199, Symbol: 'TSLA'}, 'stocks');
+    updateDocFromCollection('4pYgcAx7A987ehkH7PFm ', { Ask: 10}, 'stocks');
     // eslint-disable-next-line
   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Tradersim</h1>
       </header>
       <main className="App-main">
-        {
-          docs.map((doc) => <li key={doc.id}>{doc.Name} {doc.Balance}</li>)
-        }
       </main>
     </div>
   );
+
 }
 
 export default App;
--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -2,7 +2,7 @@
  * Contains all the functions that interacts with the Cloud Firestore database
  */
 
-import { collection, getDocs } from 'firebase/firestore';
+import { doc, addDoc, collection, getDocs, updateDoc } from 'firebase/firestore';
 import { db } from './config';
 
 /**
@@ -12,4 +12,17 @@ import { db } from './config';
 export async function getDocsFromCollection(collectionName) {
     const data = await getDocs(collection(db, collectionName));
     return data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+}
+
+/**
+ * @param doc the document object to be added to the collection
+ * @param collectionName name of the collection where document objects are added to
+ */
+export async function addDocToCollection(doc, collectionName) {
+    await addDoc(collection(db, collectionName), doc);
+}
+
+export async function updateDocFromCollection(docName, change, collectionName) {
+    const ref = doc(db, collectionName, docName);
+    await updateDoc(ref, change);
 }
